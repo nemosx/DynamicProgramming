d89af049d2e891aafea5b33612526e76c6109265
nemosx <root.michael.r@gmail.com>
2018-03-14 19:45:02 -0700

    Format code

--- rope/rope.cpp
+++ rope/rope.cpp
@@ -1,18 +1,17 @@
-#include<iostream>
-#include<algorithm>
+#include <algorithm>
+#include <iostream>
 
-int
-main() {
+int main() {
   const int n = 10;
-  int opt[n+1] = {-1};
+  int opt[n + 1] = {-1};
   opt[1] = 1;
 
   for (int i = 2; i <= n; ++i) {
     for (int k = 1; k < i; ++k) {
-      opt[i] = std::max(opt[i], std::max(opt[k] * (i - k),k * (i - k)));
+      opt[i] = std::max(opt[i], std::max(opt[k] * (i - k), k * (i - k)));
     }
   }
 
-  std::cout<<"Opt: " << opt[n];
+  std::cout << "Opt: " << opt[n];
   return 0;
 }
