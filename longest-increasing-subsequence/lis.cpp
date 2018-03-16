052d813eef896a31214e2260341c56837d1a6bee
nemosx <root.michael.r@gmail.com>
2018-03-15 19:47:47 -0700

    Refactor to use std::array

--- longest-increasing-subsequence/lis.cpp
+++ longest-increasing-subsequence/lis.cpp
@@ -1,21 +1,23 @@
-#include <iostream>
 #include <algorithm>
+#include <array>
+#include <iostream>
 
-int
-main() {
-  const int n = 7; 
-  int sequence [n] = {5, 2, 8, 9, 11, 13, 12};
-  int longestSubsequence [n];
-  std::fill_n(longestSubsequence, n, 1);
+int main() {
+  auto sequence = std::array{5, 2, 8, 9, 11, 13, 12};
+  decltype(sequence) longestSubsequence;
+  longestSubsequence.fill(1);
 
-  for (int i = 0; i < n; ++i) {
+  for (int i = 0; i < sequence.size(); ++i) {
     for (int j = 0; j < i; ++j) {
-      if (sequence[j] < sequence[i] && (longestSubsequence[j] + 1) > longestSubsequence[i]) {
-         longestSubsequence[i] = longestSubsequence[j] + 1; 
+      if (sequence[j] < sequence[i] &&
+          (longestSubsequence[j] + 1) > longestSubsequence[i]) {
+        longestSubsequence[i] = longestSubsequence[j] + 1;
       }
     }
   }
 
-  std::cout<< "Longest Subsequence: " << *std::max_element(std::begin(longestSubsequence), std::end(longestSubsequence)); 
+  std::cout << "Longest Subsequence: "
+            << *std::max_element(std::begin(longestSubsequence),
+                                 std::end(longestSubsequence));
   return 0;
 }
